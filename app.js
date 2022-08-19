89e059447bf7e5abb0828e56fd63a70dd5f90fbc
VongNgaGiun <hycj221@gmail.com>
2022-08-19 15:40:13 +0800

    add comment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//tutorial source: https://www.youtube.com/watch?v=edOmvng5IQc&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=31&ab_channel=TheNetNinja
 //app.js is the entry point for our application, so it's going to fire when the application first starts
 var express = require('express'); //require the express module
 var todoController = require('./controllers/todoController'); //require the todoController module/controller
